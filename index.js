cd0ad0b1d2a4b964ea1a7f00e18fe3b5b595805e
pnguyen12 <peterndevelopment@gmail.com>
2019-03-13 23:56:34 -0700

    init(project): project-start

---
+++ index.js
@@ -0,0 +1,42 @@
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+let lastChecked;
+let isBetween = false;
+/**
+ * 
+ * @param {shiftKey} {event: {shiftkey: true || false }}
+ */
+function onHandleCheck({ shiftKey }) {
+    console.log(shiftKey);
+    if (isBetween) resetIsBetween();
+    if (shiftKey && !this.checked) {
+        checkboxes.forEach(check => {
+            if (check === this) {
+                isBetween = !isBetween;
+            }
+            if (isBetween) {
+                check.checked = false;
+            }
+        })
+    }
+    if (shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            // last check is always first
+            // then we check if the current checkbox is the clicked checkbox to set the flags in between
+            if (checkbox === this || checkbox === lastChecked) {
+                isBetween = !isBetween;
+            }
+            if (isBetween) {
+                checkbox.checked = true;
+            }
+        })
+    }
+    lastChecked = this;
+}
+
+function resetIsBetween() {
+    isBetween = false;
+}
+window.onload = function () {
+    checkboxes.forEach(checkbox => checkbox.addEventListener('click', onHandleCheck))
+};
